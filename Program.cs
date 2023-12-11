3f3705e923dff605c464331d3940c1d4984cdf72
Lisbeth Jahre Haugnes <141266129+Turtlee19996@users.noreply.github.com>
2023-12-11 14:18:14 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Velg et tall mellom 1 - 20");
+var input = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine($"Dette er {input}");
+
+Random random = new Random();
+var randomNumber = random.Next(1, 20);
+if (input > randomNumber)
+{
+    Console.WriteLine("Dette er for høyt");
+}
+else if (input < randomNumber)
+{
+    Console.WriteLine("Dette tallet er for lavt");
+}
+else
+{
+    Console.WriteLine("Dette tallet er riktig");
+}
+
+
